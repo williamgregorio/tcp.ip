dc3c651fe1d771f2fa2ad0d11a9fe9eabf4f7567
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 12:46:25 -0400

    constants prep

--- server.c
+++ server.c
@@ -4,6 +4,8 @@
 // int socks endpoint hand
 // this will try to listen to incoming connections
 
+#define PORT 7777
+#define BUFFER_SIZE 1024 //kilobyte - we can set for msg buffer max, not sure completely
 
 int main() {
   return 0;
