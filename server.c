987358922eca0348b8ed1212c835f1fe99b7d32b
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 12:34:19 -0400

    basic scope plan, need to understand imports and proper usage

--- server.c
+++ server.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+
+// ip addressing and routing packets to destination
+// int socks endpoint hand
+// this will try to listen to incoming connections
+
+
+int main() {
+  return 0;
+}
