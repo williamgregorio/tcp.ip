c239752af9da0e91bfa0d88e689d4fe1816b27eb
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 13:26:16 -0400

    could use long, but not familiar

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 // ip addressing and routing packets to destination
 // int socks endpoint hand
@@ -9,9 +10,8 @@
 #define BUFFER_SIZE 1024 //kilobyte - we can set for msg buffer max, not sure completely
 
 int main() {
-  printf("PORT: %d\n", PORT);
-
   char buffer[BUFFER_SIZE] = {0};
   int new_socket, server_file_descriptor;
+  ssize_t nbyteread;
   return 0;
 }
