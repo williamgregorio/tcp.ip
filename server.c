1109e8e4db3e8301ee28a79073d27bd178ec8f17
William Gregorio <mrwilliamgregorio@yahoo.com>
2025-04-04 13:18:48 -0400

    zero on buffer

--- server.c
+++ server.c
@@ -8,5 +8,9 @@
 #define BUFFER_SIZE 1024 //kilobyte - we can set for msg buffer max, not sure completely
 
 int main() {
+  printf("PORT: %d\n", PORT);
+
+  char buffer[BUFFER_SIZE] = {0};
+  int new_socket;
   return 0;
 }
